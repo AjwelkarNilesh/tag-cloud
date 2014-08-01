2caf134588dc6d5a8af396b0fabdf09f7ae177a2
Nilesh <fab@ubuntu.(none)>
2014-08-02 00:19:42 +0530

    Tag Cloud Worked

--- app/controllers/tag_cloud_controller.rb
+++ app/controllers/tag_cloud_controller.rb
@@ -1,14 +1,17 @@
 class TagCloudController < ApplicationController
   def index
     weight = [1,2,3,4,5,6,7,8,9,10]
-    tags = ["Minakshi","Rashmi","Rupali","Deepali","Rajeshree","Seema","Manoj","Amit","Pankaj","Nilesh"]
-    @tags_for_cloud = []
-    10.times.each do |i|
+    tag_names = ["Minakshi","Rashmi","Rupali","Deepali","Rajeshree","Seema","Manoj","Amit","Pankaj","Nilesh"]
+    tags = []
+    10.times.each_with_index do |i, index|
       rand_weight = weight.shuffle.first
       tag_name = tags[i]
-      @tags_for_cloud << {'text' => tags[i], 'weight'=>rand_weight}
+      options_hash = {'text' => tag_names[i], 'weight'=>rand_weight}
+      options_hash.merge!({"html" => {"class"=> "vertical"}}) if (index % 3) < 1
+      tags << options_hash
       weight.delete(rand_weight)
     end
+    @tags_for_cloud = tags.to_json.html_safe
   end
 end
 
